2ddfe1d615ff3ef2d10474a2ffb23d11fc7f54c4
knraju <knraju.bsc@gmail.com>
2017-08-29 13:00:03 +0530

    first push to trigger the job

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("this is test for java project");
+		System.out.println("this is test for java project pushed first time to trigger jenkins job");
 	}
 
 }
