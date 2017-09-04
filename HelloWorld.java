946a3c3e4589d8406a4aeb7ae4f935f16acfefda
knraju <knraju.bsc@gmail.com>
2017-09-04 20:14:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("this is test for java project pushed first time to trigger jenkins job test");
 		System.out.println("this is for pull request demo");
 		System.out.println("this is for pull request demooooooooooooooooooo");
-		//Comment1
+		//Comment2
 	}
 
 }
